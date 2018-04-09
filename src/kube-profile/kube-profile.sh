30a40d3f62d54225b66a944a1ff30bb3ed667954
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2018-04-09 12:18:43 -0700

    typos

--- src/kube-profile/kube-profile.sh
+++ src/kube-profile/kube-profile.sh
@@ -2,10 +2,10 @@
 KUBEADMIN=/etc/kubernetes/admin.conf
 if [ $(/usr/bin/id -u) == "0" ]; then
 	if [ "x$KUBECONFIG" == "x" ]; then 
-		export KUBECONFIG=$(KUBEADMIN)
+		export KUBECONFIG=$KUBEADMIN
 	fi
 else
-	[ ! -d $HOME/.kube ]; then
-		export KUBECONFIG=$(KUBEADMIN)
+	if [ ! -d $HOME/.kube ]; then
+		export KUBECONFIG=$KUBEADMIN
 	fi
 fi

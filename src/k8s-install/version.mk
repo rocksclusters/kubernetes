bbee5f9a2fd5522ba41c06bd0df21478394e2227
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2018-04-30 15:42:44 -0700

    Small build fixups

--- src/k8s-install/version.mk
+++ src/k8s-install/version.mk
@@ -3,6 +3,7 @@ NAME		= k8s-install
 RELEASE		= 0
 VERSION		= $(K8SVERSION)
 PKGROOT		= /opt/rocks
-RPM.FILES	= $(PKGROOT)/*
+RPM.FILES	= $(PKGROOT)/doc/*\n\
+		  $(PKGROOT)/sbin/*
 RPM.DESCRIPTION = install instructions for kubernetes
 RPM.ARCH	= noarch

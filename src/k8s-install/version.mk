9bbd1f3ea899430e2a62cb5361df16d23129b0de
root <root@lima-vc-4.sdsc.optiputer.net>
2018-04-30 15:36:39 -0700

    Think I configured configure script to install to sbin

--- src/k8s-install/version.mk
+++ src/k8s-install/version.mk
@@ -2,7 +2,7 @@
 NAME		= k8s-install
 RELEASE		= 0
 VERSION		= $(K8SVERSION)
-PKGROOT		= /opt/rocks/doc
+PKGROOT		= /opt/rocks
 RPM.FILES	= $(PKGROOT)/*
 RPM.DESCRIPTION = install instructions for kubernetes
 RPM.ARCH	= noarch
